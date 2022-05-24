9564f36ca2d9620f1bfd200ca7503ecba89c64bf
Gustavo Santos <toltech99@gmail.com>
2022-05-25 01:11:45 +0200

    database user changed

--- config/config.php
+++ config/config.php
@@ -5,9 +5,11 @@ $dbParams = [
     'driver' => 'pdo_mysql',
     'host' => '127.0.0.1',
     'dbname' => 'entrades',
-    'user' => 'usr_generic',
+    'user' => 'usr_consulta', // o 'usr_generic': l'usuari ha de tenir permisos de SELECT a la base de dades.
     'password' => 'Thico@2020'
 ];
+
+// La contrasenya per obrir el PDF s'estableix a ControladorEntrada.class.php, Línia 50: $mpdf->SetProtection(array(), '1234');
 
 // Estamos en modo desarrollo?
 $dev = true;
